b8ff69e4e3dc19feb5300a5352a6d5d62c246ab1
AndreyIsrael <andreyisrael2000@gmail.com>
2026-04-25 02:48:49 -0300

    serializers - response objects - Nested serializers - SerializerMethodField - serializer relationships

--- api/models.py
+++ api/models.py
@@ -36,7 +36,7 @@ class Order(models.Model):
     products = models.ManyToManyField(Product, through="OrderItem", related_name='orders')
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
