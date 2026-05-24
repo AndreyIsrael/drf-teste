b11c634e78b249345329bfc4d678b1ee005edc03
AndreyIsrael <andreyisrael2000@gmail.com>
2026-05-24 03:00:16 -0300

    PageNumberPagination & LimitOffsetPagination

--- api/views.py
+++ api/views.py
@@ -11,6 +11,7 @@ from rest_framework.views import APIView
 from api.filters import ProductFilter, InStockFilterBackend
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
 
 # class ProductCreateAPIView(generics.CreateAPIView):
 #     model = Product
@@ -20,7 +21,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 #         return super().create(request, *args, **kwargs)
     
 class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
+    queryset = Product.objects.order_by('pk')
     serializer_class = ProductSerializer
     filterset_class = ProductFilter
     filter_backends = [
@@ -32,6 +33,11 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 
     search_fields=['=name', 'description']
     ordering_fields = ['name', 'price', 'stock']
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 2 
+    pagination_class.page_query_param = 'pagenum'
+    pagination_class.page_size_query_param = 'size'
+    pagination_class.max_page_size = 6 
  
     def get_permissions(self):
         self.permission_classes = [AllowAny]
